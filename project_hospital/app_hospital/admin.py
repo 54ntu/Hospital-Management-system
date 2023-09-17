ceb8051f5bd2143cb2d795c3c295882f565c2e54
54ntu <chaudharysantaram8@gmail.com>
2023-09-17 22:16:56 +0545

    patient model & superuser is created

--- project_hospital/app_hospital/admin.py
+++ project_hospital/app_hospital/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import patientModel
 
 # Register your models here.
+class  patientAdminModel(admin.ModelAdmin):
+    list_display= ('firstname','lastname','age','sex')
+
+
+
+admin.site.register(patientModel,patientAdminModel)
--- project_hospital/app_hospital/models.py
+++ project_hospital/app_hospital/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class patientModel(models.Model):
+    firstname = models.CharField(max_length=100,null=False,blank=False)
+    lastname = models.CharField(max_length=100,null=False,blank=False)
+    age = models.IntegerField(null=False,blank=False)
+    sex = models.CharField(max_length=50, null=False,blank=False)
+
+
+    def __str__(self):
+        return self.firstname
+    
